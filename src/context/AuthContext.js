ea54a1729cc27b2ce83241203ceb4a710f776a9c
olcay-elt <olcay.elt@gmail.com>
2023-04-01 15:00:55 +0200

    Remove unnecessary lines

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,20 +1,18 @@
 import {
-  createUserWithEmailAndPassword,
-  GoogleAuthProvider,
-  onAuthStateChanged,
-  sendPasswordResetEmail,
-  signInWithEmailAndPassword,
-  signInWithPopup,
-  signOut,
-  updateProfile,
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    sendPasswordResetEmail,
+    signInWithPopup,
+    signOut,
+    updateProfile,
 } from "firebase/auth";
 import React, { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../auth/firebase";
 import {
-  toastErrorNotify,
-  toastSuccessNotify,
-  toastWarnNotify,
+    toastErrorNotify,
+    toastSuccessNotify,
+    toastWarnNotify,
 } from "../helpers/ToastNotify";
 
 export const AuthContext = createContext();
@@ -25,112 +23,112 @@ export const AuthContext = createContext();
 //   };
 
 const AuthContextProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState("");
-  const navigate = useNavigate();
+    const [currentUser, setCurrentUser] = useState("");
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    userObserver();
-  }, []);
+    useEffect(() => {
+        userObserver();
+    }, []);
 
-  const createUser = async (email, password, displayName) => {
-    try {
-      //? yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      //? kullanıcı profilini güncellemek için kullanılan firebase metodu
-      await updateProfile(auth.currentUser, {
-        displayName: displayName,
-      });
-      navigate("/");
-      toastSuccessNotify("Registered successfully!");
-      //   console.log(userCredential);
-    } catch (error) {
-      toastErrorNotify(error.message);
-    }
-  };
+    const createUser = async (email, password, displayName) => {
+        try {
+            //? yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
+            //   const userCredential = await createUserWithEmailAndPassword(
+            //     auth,
+            //     email,
+            //     password
+            //   );
+            //? kullanıcı profilini güncellemek için kullanılan firebase metodu
+            await updateProfile(auth.currentUser, {
+                displayName: displayName,
+            });
+            navigate("/");
+            toastSuccessNotify("Registered successfully!");
+            //   console.log(userCredential);
+        } catch (error) {
+            toastErrorNotify(error.message);
+        }
+    };
 
-  //* https://console.firebase.google.com/
-  //* => Authentication => sign-in-method => enable Email/password
-  //! Email/password ile girişi enable yap
-  const signIn = async (email, password) => {
-    try {
-      //? mevcut kullanıcının giriş yapması için kullanılan firebase metodu
-      let userCredential = await signInWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      navigate("/");
-      toastSuccessNotify("Logged in successfully!");
-      //   console.log(userCredential);
-    } catch (error) {
-      toastErrorNotify(error.message);
-    }
-  };
+    //* https://console.firebase.google.com/
+    //* => Authentication => sign-in-method => enable Email/password
+    //! Email/password ile girişi enable yap
+    const signIn = async (email, password) => {
+        try {
+            //? mevcut kullanıcının giriş yapması için kullanılan firebase metodu
+            // let userCredential = await signInWithEmailAndPassword(
+            //     auth,
+            //     email,
+            //     password
+            // );
+            navigate("/");
+            toastSuccessNotify("Logged in successfully!");
+            //   console.log(userCredential);
+        } catch (error) {
+            toastErrorNotify(error.message);
+        }
+    };
 
-  const logOut = () => {
-    signOut(auth);
-  };
+    const logOut = () => {
+        signOut(auth);
+    };
 
-  const userObserver = () => {
-    //? Kullanıcının signin olup olmadığını takip eden ve kullanıcı değiştiğinde yeni kullanıcıyı response olarak dönen firebase metodu
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // console.log(user);
-        const { email, displayName, photoURL } = user;
-        setCurrentUser({ email, displayName, photoURL });
-      } else {
-        setCurrentUser(false);
-        // console.log("user logged out");
-      }
-    });
-  };
+    const userObserver = () => {
+        //? Kullanıcının signin olup olmadığını takip eden ve kullanıcı değiştiğinde yeni kullanıcıyı response olarak dönen firebase metodu
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // console.log(user);
+                const { email, displayName, photoURL } = user;
+                setCurrentUser({ email, displayName, photoURL });
+            } else {
+                setCurrentUser(false);
+                // console.log("user logged out");
+            }
+        });
+    };
 
-  //* https://console.firebase.google.com/
-  //* => Authentication => sign-in-method => enable Google
-  //! Google ile girişi enable yap
-  //* => Authentication => settings => Authorized domains => add domain
-  //! Projeyi deploy ettikten sonra google sign-in çalışması için domain listesine deploy linkini ekle
-  const signUpProvider = () => {
-    //? Google ile giriş yapılması için kullanılan firebase metodu
-    const provider = new GoogleAuthProvider();
-    //? Açılır pencere ile giriş yapılması için kullanılan firebase metodu
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        console.log(result);
-        navigate("/");
-        toastSuccessNotify("Logged in successfuly!");
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+    //* https://console.firebase.google.com/
+    //* => Authentication => sign-in-method => enable Google
+    //! Google ile girişi enable yap
+    //* => Authentication => settings => Authorized domains => add domain
+    //! Projeyi deploy ettikten sonra google sign-in çalışması için domain listesine deploy linkini ekle
+    const signUpProvider = () => {
+        //? Google ile giriş yapılması için kullanılan firebase metodu
+        const provider = new GoogleAuthProvider();
+        //? Açılır pencere ile giriş yapılması için kullanılan firebase metodu
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                console.log(result);
+                navigate("/");
+                toastSuccessNotify("Logged in successfuly!");
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
-  const forgotPassword = (email) => {
-    //? Email yoluyla şifre sıfırlama için kullanılan firebase metodu
-    sendPasswordResetEmail(auth, email)
-      .then(() => {
-        // Password reset email sent!
-        toastWarnNotify("Please check your mail box!");
-      })
-      .catch((err) => {
-        toastErrorNotify(err.message);
-        // ..
-      });
-  };
+    const forgotPassword = (email) => {
+        //? Email yoluyla şifre sıfırlama için kullanılan firebase metodu
+        sendPasswordResetEmail(auth, email)
+            .then(() => {
+                // Password reset email sent!
+                toastWarnNotify("Please check your mail box!");
+            })
+            .catch((err) => {
+                toastErrorNotify(err.message);
+                // ..
+            });
+    };
 
-  const values = {
-    createUser,
-    signIn,
-    logOut,
-    signUpProvider,
-    forgotPassword,
-    currentUser,
-  };
-  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
+    const values = {
+        createUser,
+        signIn,
+        logOut,
+        signUpProvider,
+        forgotPassword,
+        currentUser,
+    };
+    return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
 
 export default AuthContextProvider;
