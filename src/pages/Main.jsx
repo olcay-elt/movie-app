eb5c6e147d632972d077e1ac784d9e9c344f0e90
olcay-elt <olcay.elt@gmail.com>
2023-04-02 16:14:52 +0200

    Add

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -22,5 +22,25 @@ const Main = () => {
             .then(res => console.log('res: ', res))
     }
 
+    console.log(movies);
+    return (
+        <>
+            <form className="flex justify-center p-2">
+                <input
+                    type="search"
+                    className="w-80 h-8 rounded-md p-1 m-2"
+                    placeholder="Search a movie..."
+                />
+                <button className="btn-danger-bordered" type="submit">
+                </button>
+            </form>
+            <div className="flex justify-center flex-wrap">
+                {
+                    movies.map((movie) => <MovieCard key={movie.id} {...movie} />)
+                }
+            </div>
+        </>
+    );
+};
 
-    export default Main;
+export default Main;
