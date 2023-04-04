16966342b039425165de77a47df2ed2adaa4aec0
olcay-elt <olcay.elt@gmail.com>
2023-04-04 17:42:19 +0200

    Add

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -11,9 +11,6 @@ const Main = () => {
     const { movies } = useContext(MovieContext);
     const FEATURED_API = 'https://api.themoviedb.org/3/discover/movie?api_key=68815c4fd1873452233f78017cfd779d'
 
-    useEffect(() => {
-        getData(FEATURED_API)
-    }, [])
 
 
 
