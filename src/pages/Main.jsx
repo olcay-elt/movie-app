b1ff1ed6230627a3530145ee37eb518b126d41a9
olcay-elt <olcay.elt@gmail.com>
2023-04-05 13:13:04 +0200

    Add

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -47,15 +47,3 @@ const Main = () => {
 export default Main;
 
 
-import { createContext, useContext, useState } from "react";
-
-const UserContext = createContext();
-
-export const UserProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
-    const values = { user, setUser };
-    return
-    <UserContext.Provider value={values}>{children}</UserContext.Provider>
-};
-
-export const useUser = () => useContext(UserContext)
