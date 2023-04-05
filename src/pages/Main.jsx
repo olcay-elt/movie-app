1a4a418b0b4cbb29820a483d8c2b8b4546d921c9
olcay-elt <olcay.elt@gmail.com>
2023-04-05 13:09:23 +0200

    Add

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -52,5 +52,5 @@ import { createContext, useContext, useState } from "react";
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-
+    const [user, setUser] = useState(null);
 }
