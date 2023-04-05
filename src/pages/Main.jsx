012d3a00535215241eab2002bf391a70f334d57f
olcay-elt <olcay.elt@gmail.com>
2023-04-05 13:11:21 +0200

    Add

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -53,4 +53,7 @@ const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const values = { user, setUser };
+    return
+    <UserContext.Provider value={values}>{children}</UserContext.Provider>
 }
