c59c625b1887c7328a1790b8c82ca451f7f444ff
olcay-elt <olcay.elt@gmail.com>
2023-04-05 13:06:15 +0200

    Add

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -45,3 +45,10 @@ const Main = () => {
 };
 
 export default Main;
+
+
+import { createContext, useContext, useState } from "react";
+const UserContext = createContext();
+export const UserProvider = ({ children }) => {
+
+}
