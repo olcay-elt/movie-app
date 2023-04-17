b9e7796a9b28e5cbc3d1078ac8afa9df9e665ba4
olcay-elt <olcay.elt@gmail.com>
2023-04-17 23:41:18 +0200

    Add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,41 +45,41 @@ const Navbar = () => {
                                 aria-labelledby="dropdownMenuButton2"
                                 data-te-dropdown-menu-ref=""
                             >
-                                <li>
-                                    <Link
-                                        className="block w-full whitespace-nowrap bg-transparent py-2 px-4 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
-                                        to="/register"
-                                        data-te-dropdown-item-ref=""
-                                    >
-                                        Register
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        className="block w-full whitespace-nowrap bg-transparent py-2 px-4 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
-                                        to="/login"
-                                        data-te-dropdown-item-ref=""
-                                    >
-                                        Login
-                                    </Link>
-                                </li>
-                                <li>
-                                    <span
-                                        className="block w-full whitespace-nowrap bg-transparent py-2 px-4 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
-                                        role="button"
-                                        data-te-dropdown-item-ref=""
-                                        onClick={() => logOut()}
-                                    >
-                                        Logout
-                                    </span>
-                                </li>
-                            </ul>
-                        </div>
+
+                                <Link
+                                    className="block w-full whitespace-nowrap bg-transparent py-2 px-4 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
+                                    to="/register"
+                                    data-te-dropdown-item-ref=""
+                                >
+                                    Register
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    className="block w-full whitespace-nowrap bg-transparent py-2 px-4 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
+                                    to="/login"
+                                    data-te-dropdown-item-ref=""
+                                >
+                                    Login
+                                </Link>
+                            </li>
+                            <li>
+                                <span
+                                    className="block w-full whitespace-nowrap bg-transparent py-2 px-4 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
+                                    role="button"
+                                    data-te-dropdown-item-ref=""
+                                    onClick={() => logOut()}
+                                >
+                                    Logout
+                                </span>
+                            </li>
+                        </ul>
                     </div>
-                    {/* Right elements */}
                 </div>
+                {/* Right elements */}
+        </div>
             </nav >
-            <div className="h-[52px]"></div>
+    <div className="h-[52px]"></div>
         </div >
     );
 };
